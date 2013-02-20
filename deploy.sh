91cd90845014de3d1aadb6b250a8257b756be7c0
GoogolMo <googolmo@gmail.com>
2013-02-20 23:28:32 +0800

    update .gitignore

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-cd ./librarywithsherlock/
-mvn -Phome -DaltDeploymentRepository=snapshot-repo::default::file:/Users/googolmo/project/lib/rep/snapshots clean deploy
-cd /Users/googolmo/project/lib/rep/
-git status
-git add .
-git commit -a -m'update slidingmenu'
-git push origin master

6de22933abe925054efe587b5a436c36e27dde8b
GoogolMo <googolmo@gmail.com>
2013-03-06 22:14:17 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-mvn -Phome -DaltDeploymentRepository=snapshot-repo::default::file:/Users/googolmo/project/lib/rep/snapshots clean deploy
-cd /Users/googolmo/project/lib/rep/
-rm -rvf /Users/googolmo/project/lib/rep/snapshots/com/slidingmenu/example/
+mvn -Phome -DaltDeploymentRepository=snapshot-repo::default::file:/Users/googolmo/project/project/lib/rep/snapshots clean deploy
+cd /Users/googolmo/project/project/lib/rep/
+rm -rvf /Users/googolmo/project/project/lib/rep/snapshots/com/slidingmenu/example/
 git status
 git add .
 git commit -a -m'update slidingmenu'
